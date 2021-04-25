a4ebd86886177c15d439449882bfa83b7dc861cc
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-04-25 16:48:42 +0300

    save sereen

---
+++ java/code.js
@@ -0,0 +1,15 @@
+'use strict';
+// let num1 = parseInt(prompt('How many books you read in month?');
+
+var num1 = parseInt(prompt('How many books you read in month?'); 
+document.write(`<span >welcome, you read ${num1}</span>`);
+
+let numbook = prompt( 'Anytype of books did you prefer? novels or politcal?');
+document.write(`<span > and, you prefer ${numbook}</span>`);
+
+let name1 = prompt('Tell me the name of your favourite book?');
+document.write(`<span >  your favourite book is ${numbook}</span>`);
+
+let type =prompt('Did you prefer to listen to the books or read ? ');
+document.write(`<span > and you prefer to ${name1}</span>`);
+
